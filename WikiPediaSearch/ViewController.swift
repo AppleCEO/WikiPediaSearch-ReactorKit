970914a9fe231b7dbec4d92e72713b37ece05321
도미닉 <kiljh@me.com>
2020-05-17 15:56:00 +0900

    create project

---
+++ WikiPediaSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiPediaSearch
+//
+//  Created by joon-ho kil on 2020/05/17.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
